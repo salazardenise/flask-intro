f0dd3585576315a2871bfff7b4aa583941e7c94d
Denise Salazar <denise.salazar.1210@gmail.com>
2018-09-18 21:59:47 -0700

    Add host argument to app.run()

--- server.py
+++ server.py
@@ -85,4 +85,4 @@ def greet_person():
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
     # our web app if we change the code.
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
